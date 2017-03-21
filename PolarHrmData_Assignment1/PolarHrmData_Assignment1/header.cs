3221aa6a930525a40e72596dfcf4903f9543e60b
Prashid Pradhan <stropher@gmail.com>
2017-03-21 21:35:29 +0545

    Brought in two lines

---
+++ PolarHrmData_Assignment1/PolarHrmData_Assignment1/header.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarHrmData_Assignment1
+{
+    class header
+    {
+        public string headername { get; set; }
+        public int headerline { get; set; }
+    }
+}
